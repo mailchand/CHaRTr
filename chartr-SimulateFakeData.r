56e5e4e1a6edc67c7e0231bea5c1750de8a9dafc
Chand <mailchand@gmail.com>
2019-03-05 23:30:22 -0500

    [Chand] Adding new simulators

--- chartr-SimulateFakeData.r
+++ chartr-SimulateFakeData.r
@@ -1,21 +1,27 @@
 # demo script for generating data from one of the models 
 # that will be fit with the other model
-source("chartr-helperfunctions.r")
-source("simulators.r")
+source("chartr-HelperFunctions.r")
+
 nCoh = 5; # Create data with 7 coherence
 nmc = 500; # Take 500 trials for simulations
+saveFiles = FALSE;
+saveDir = "caseStudy2"
+subjectPrefix = "Subj"
 
 model = "bUGMSv"
-for(nSubj in c(2)){
+par(mfrow=c(2,3))
+for(nSubj in seq(1,5)){
   fP = paramsandlims(model,nCoh, fakePars = TRUE)
   currParams = fP$fakeParams
   currParams["aU"] = 4900;
   currParams["eta"] = 5;
-  currParams["intercept"] = 200 + rnorm(1,0,1);
+  currParams["intercept"] = 200 + 20*rnorm(1,0,1);
 
   dat = simulateRTs(model, currParams , n=nmc, nds=nCoh)
   
-  saveFileName=paste("Example4/","Subj",nSubj,sep="");
-  save(file=saveFileName,dat, currParams, genModel=model);
+  saveFileName=paste(saveDir,subjectPrefix,nSubj,sep="");
+  if(saveFiles){
+   # save(file=saveFileName,dat, currParams, genModel=model);
+  }
 }
 
